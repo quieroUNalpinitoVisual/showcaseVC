b38bbfc8e2ac763ae7810922a20cc7e4e9b38b79
jsromero98 <84394100+jsromero98@users.noreply.github.com>
2022-03-31 15:14:24 -0500

    agregada imagen base en canvas p5

--- content/docs/shortcodes/masking.md
+++ content/docs/shortcodes/masking.md
@@ -1,5 +1,5 @@
 # Masking
 
-Empezamos con una imagen base:
+Empezamos con una imagen base   
 
-{{< p5-div ver="1.4.1" sketch="/showcasevc/p5files/masking/masking1.js" lib1="https://cdntolib1/lib1.js" >}}
+{{< p5-div sketch="/showcasevc/p5files/masking/masking1.js" >}}
--- content/p5files/masking/masking1.js
+++ content/p5files/masking/masking1.js
@@ -1,7 +1,11 @@
-function setup() {
-    createCanvas(400, 400);
-  }
+
+new p5((p) => {
+    p.setup = function () {
+        p.createCanvas(400,400);
+        p.loadImage('/showcasevc/sketches/lenna.png', img => {
+            img.resize(400,400);
+            p.image(img, 0, 0);
+          });
+    };
+  }, "masking1");
   
-  function draw() {
-    background(150);
-  }
