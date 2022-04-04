4159513c2919c71e00b146805eea37c93c2b94ed
Daniel Arturo Moreno Rincon <damorenor@unal.edu.co>
2022-04-04 18:53:46 -0500

    terrain relief coloring has been created :fire:

--- content/sketches/terrain.js
+++ content/sketches/terrain.js
@@ -5,6 +5,7 @@ new p5((p) => {
   let cols, rows;
   let terrain = [];
   let flying = 0;
+  let changeColor, change, c1 = p.color(144, 247, 124), c2 = p.color(138, 95, 22);
 
   p.setup = function () {
     p.createCanvas(400, 400, p.WEBGL);
@@ -38,9 +39,11 @@ new p5((p) => {
     p.translate(-w/2,-h/2);
 
     for(let y = 0; y < rows-1; y++){
-      p.fill('rgba(0,'+255+',0, 0.25)');
       p.beginShape(p.TRIANGLE_STRIP);
       for(let x = 0; x < cols; x++){
+        change = p.map(terrain[x][y],-100,100,0,1);
+        changeColor = p.lerpColor(c1,c2,change);
+        p.fill(changeColor);
         p.vertex(x*scl, y*scl, terrain[x][y]);
         p.vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
       }
